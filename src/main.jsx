efe5fea578a224cdad0e16fa573a708d75621311
Nastya <lobash.n@gmail.com>
2025-04-02 18:52:18 +0300

    to work

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store.js";
+import { store, persistor } from "./redux/storage.js";
 import "./index.css";
 import App from "./components/App/App.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
